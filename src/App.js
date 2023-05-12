b8589c47a0875789294068be7b36418f860b50d0
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-05-12 10:42:24 +0530

    Initial commit with React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-link">Hi</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
